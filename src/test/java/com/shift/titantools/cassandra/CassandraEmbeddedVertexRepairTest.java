8044ed9fac245517e5dfaf13af9ace9130ac2bdd
Blake Eggleston <bdeggleston@gmail.com>
2013-04-06 18:52:10 -0700

    adding vertex repair test for cassandra embedded

---
+++ src/test/java/com/shift/titantools/cassandra/CassandraEmbeddedVertexRepairTest.java
@@ -0,0 +1,24 @@
+package com.shift.titantools.cassandra;
+
+import com.shift.titantools.base.VertexRepairTest;
+import org.apache.commons.configuration.BaseConfiguration;
+import org.apache.commons.configuration.Configuration;
+
+import static com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration.*;
+import static com.thinkaurelius.titan.diskstorage.cassandra.embedded.CassandraEmbeddedStoreManager.*;
+
+public class CassandraEmbeddedVertexRepairTest extends VertexRepairTest {
+    public static Configuration getConfig() {
+        Configuration config = new BaseConfiguration();
+        config.subset(STORAGE_NAMESPACE).addProperty(STORAGE_BACKEND_KEY, "embeddedcassandra");
+        config.subset(STORAGE_NAMESPACE).addProperty(
+                CASSANDRA_CONFIG_DIR_KEY,
+                "file://" + System.getProperty("user.dir") + "/src/test/resources/config/cassandra.yaml"
+        );
+        return config;
+    }
+
+    public CassandraEmbeddedVertexRepairTest() {
+        super(getConfig());
+    }
+}
